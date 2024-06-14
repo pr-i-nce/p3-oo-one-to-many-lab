8dadf0504f7fa24a8f3dc5746533a9476d9d27cd
prince mumo <princeinkunzi@gmail.com>
2024-06-14 16:50:36 +0300

    first

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,78 @@
-class Pet:
-    pass
+# lib/data_structures.py
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Only instances of Pet can be added.")
+        if pet.owner is not self:
+            pet.owner = self
+        if pet not in self._pets:
+            self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"{pet_type} is not a valid pet type.")
+        self.name = name
+        self.pet_type = pet_type
+        self._owner = None  # Use a private attribute to manage owner
+
+        if owner:
+            self.owner = owner
+
+        Pet.all.append(self)
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        if not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of Owner.")
+        if self._owner is not owner:
+            if self._owner:
+                self._owner._pets.remove(self)
+            self._owner = owner
+            owner.add_pet(self)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
